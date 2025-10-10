a116ff62160c797a5b0d9dab486ee5b98d7b7ff2
Timoté Ballochi <tballochi99@gmail.com>
2025-10-10 11:14:38 +0200

    feat(test): auth

---
+++ back/tests/setup_test.go
@@ -0,0 +1,33 @@
+package tests
+
+import (
+	"testing"
+
+	"github.com/gin-gonic/gin"
+	"github.com/stretchr/testify/require"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+
+	"back/database"
+	"back/models"
+)
+
+func setupTestDB(t *testing.T) {
+	db, err := gorm.Open(sqlite.Open(":memory:"), &gorm.Config{})
+	require.NoError(t, err, "Erreur lors de la création de la DB de test")
+
+	err = db.AutoMigrate(&models.User{})
+	require.NoError(t, err, "Erreur lors de la migration")
+
+	database.DB = db
+}
+
+func cleanupTestDB(t *testing.T) {
+	err := database.DB.Exec("DELETE FROM users").Error
+	require.NoError(t, err)
+}
+
+func setupTestRouter() *gin.Engine {
+	gin.SetMode(gin.TestMode)
+	return gin.New()
+}
