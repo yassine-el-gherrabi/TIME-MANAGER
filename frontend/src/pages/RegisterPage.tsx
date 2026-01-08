79dd04c441f1ec8083ac3c90693a4261b1695500
Yassine EL GHERRABI <yassineelgherrabi@gmail.com>
2026-01-08 19:13:15 +0100

    chore(cleanup): remove RegisterPage (part of auth refactoring)

--- frontend/src/pages/RegisterPage.tsx
+++
@@ -1,9 +0,0 @@
-import { RegisterForm, AuthLayout } from '../components/auth';
-
-export function RegisterPage() {
-  return (
-    <AuthLayout title="Create Account">
-      <RegisterForm />
-    </AuthLayout>
-  );
-}
